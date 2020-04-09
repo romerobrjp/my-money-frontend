be07b62e84578300eddddcf019421a65a4c96c5a
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-09 12:20:05 -0300

    WIP: introduces the Menu and Sidebar components

--- src/main/app.jsx
+++ src/main/app.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import '../common/template/dependencies'
 import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
 
 export default (props) => (
   <div className="wrapper">
     <Header />
+    <Sidebar />
   </div>
 )
