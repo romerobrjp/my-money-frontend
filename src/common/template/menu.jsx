97c94e43e0e0256009c154913b0b7df90967c73a
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-09 12:29:22 -0300

    WIP: adds MenuITree and MenuItem components

--- src/common/template/menu.jsx
+++ src/common/template/menu.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
+import MenuTree from './menuTree'
+import MenuItem from './menuItem'
 
-export default (props) => (
+export default props => (
   <ul className='sidebar-menu'>
+    <MenuItem path='/' label='Dashboard' icon='dashboard' />
+
+    <MenuTree label='Register' icon='edit'>
+      <MenuItem path='billingCycles' label='Billing Cycle' icon='usd' />
+    </MenuTree>
   </ul>
-);
+)
