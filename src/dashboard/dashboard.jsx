5e0c2b77d5dc6f718cc06188db43036ae8144fa6
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-12 21:28:45 -0300

    Adds Row and ValueBox components into Dashboard

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import Row from '../common/layout/row'
+import ValueBox from '../common/widget/valueBox'
 
 class Dashboard extends Component {
 render() {
@@ -9,7 +11,11 @@ render() {
         <ContentHeader title='Dashboard' small='Beta Version' />
 
         <Content>
-            Dashboard
+        <Row>
+          <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total Credits' />
+          <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10' text='Total Debts' />
+          <ValueBox cols='12 4' color='blue' icon='money' value='R$ 0' text='Consolidated Amount' />
+        </Row>
         </Content>
       </div>
     )
