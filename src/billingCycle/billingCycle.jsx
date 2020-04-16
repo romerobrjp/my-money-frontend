10e32c9985225d152c8608a32e80b6e8ca8fb835
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-16 00:03:17 -0300

    Refactor to class a component.

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default props => (
-  <div>
-    <h1>Billing Cycle Page</h1>
-  </div>
-)
+import ContentHeader from '../common/template/contentHeader'
+import Content from '../common/template/content'
+
+export default class BillingCycle extends Component {
+  render() {
+    return (
+      <div>
+        <ContentHeader title='Billing Cycles' small='Register' />
+        <Content>
+          Billing Cycles
+        </Content>
+      </div>
+    )
+  }
+}
