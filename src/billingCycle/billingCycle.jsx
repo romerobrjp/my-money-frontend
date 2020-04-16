d236d8f8f2b33cdc8a13a0bff528c51281c1040a
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-16 00:09:12 -0300

    WIP: introduces the tabs components

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -3,13 +3,23 @@ import React, { Component } from 'react'
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
 
+import Tab from '../common/tab/tab'
+import TabHeader from '../common/tab/tabHeader'
+import TabContent from '../common/tab/tabContent'
+
 export default class BillingCycle extends Component {
   render() {
     return (
       <div>
         <ContentHeader title='Billing Cycles' small='Register' />
+
         <Content>
-          Billing Cycles
+          <Tab>
+            <TabHeader>
+              </TabHeader>
+              <TabContent>
+            </TabContent>
+          </Tab>
         </Content>
       </div>
     )
