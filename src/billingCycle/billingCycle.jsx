c065b40280ecb5b05ee19040b231173ec84dee40
Romero Medeiros <romeromedeiros5@gmail.com>
2020-04-23 00:17:53 -0300

    Uses componentWillMount to pre-select the List tab

--- src/billingCycle/billingCycle.jsx
+++ src/billingCycle/billingCycle.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -8,8 +10,13 @@ import TabsHeader from '../common/tab/tabsHeader'
 import TabHeader from '../common/tab/tabHeader'
 import TabsContent from '../common/tab/tabsContent'
 import TabContent from '../common/tab/tabContent'
+import { selectTab } from '../common/tab/tabActions'
 
-export default class BillingCycle extends Component {
+class BillingCycle extends Component {
+  componentWillMount() {
+    this.props.selectTab('tabList')
+  }
+
   render() {
     return (
       <div>
@@ -36,3 +43,6 @@ export default class BillingCycle extends Component {
     )
   }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({selectTab}, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycle)
