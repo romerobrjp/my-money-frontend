2e3049769a142d62749fe1b22fb9844248c1c163
Romero <romeromedeiros5@gmail.com>
2020-05-04 22:41:56 -0300

    Fixes typo

--- src/common/template/footer.jsx
+++ src/common/template/footer.jsx
@@ -4,9 +4,9 @@ export default props => (
   <footer className='main-footer'>
     <strong>
         Copyright &copy; {new Date().getFullYear()} - 
-        <a href='http://github.com/rom5jp' target='_blank'> rom5jp Gihub</a>
+        <a href='http://github.com/rom5jp' target='_blank'> rom5jp Github</a>
         <span> | </span>
-        <a href='http://gitlab.com/rom5jp' target='_blank'> rom5jp Gilab</a>
+        <a href='http://gitlab.com/rom5jp' target='_blank'> rom5jp Gitlab</a>
     </strong>
   </footer>
 )
