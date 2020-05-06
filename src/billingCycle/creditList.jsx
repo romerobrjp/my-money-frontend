fe94c0a8f3041fdee8320188cb99de767294e766
Romero <romeromedeiros5@gmail.com>
2020-05-05 22:06:49 -0300

    Allows to render all the credits in the CreditList component

--- src/billingCycle/creditList.jsx
+++ src/billingCycle/creditList.jsx
@@ -7,13 +7,15 @@ import Input from '../common/form/input'
 class CreditList extends React.Component {
 
   renderRows() {
-    return (
-      <tr>
-        <td><Field name='credits[0].name' component={Input} placeholder='Enter the name here' readOnly={this.props.readOnly} /></td>
-        <td><Field name='credits[0].value' component={Input} placeholder='Enter the value here' readOnly={this.props.readOnly} /></td>
+    const list = this.props.list || []
+
+    return list.map((item, index) => (
+      <tr key={index}>
+        <td><Field name={`credits[${index}].name`} component={Input} placeholder='Enter the name here' readOnly={this.props.readOnly} /></td>
+        <td><Field name={`credits[${index}].value`} component={Input} placeholder='Enter the value here' readOnly={this.props.readOnly} /></td>
         <td></td>
       </tr>
-    )
+    ))
   }
 
   render() {
